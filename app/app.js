525fb8d0e2ea034738335ab219f0b1a5bfcf7190
xact-one <marksafer@outlook.com>
2025-12-23 17:51:12 -0500

    Add login button event listener and log message

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
+console.log("App loaded");
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", () => {
+  alert("Login clicked (real auth coming later)");
+});
 
